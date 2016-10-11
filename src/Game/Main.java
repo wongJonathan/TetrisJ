b9dbb38d4b92953172ecbbe8ca84a48fdd81b2d4
Jonathan Wong <wong.jonathan@live.com>
2016-10-11 18:17:43 -0400

    Added Some Comments

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -6,7 +6,10 @@ import javax.swing.JFrame;
 public class Main extends JFrame{
 
 	public Main(){
+		// Creates the panel for display
 		add(new MainGameUpdate());
+		
+		// Sets the frame settings
 		setSize(800,800);
 		setTitle("My Tetris Game");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
