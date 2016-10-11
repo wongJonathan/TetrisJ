4740b2bd5733247cd7395623356c6866e602c7d8
Jonathan Wong <wong.jonathan@live.com>
2016-10-11 17:46:03 -0400

    Commit Initial

---
+++ src/MainGameUpdate.java
@@ -0,0 +1,20 @@
+/*
+ *  Name: MainGameUpdate
+ *  
+ *  Description: Where main loop runs all of the game
+ */
+public class MainGameUpdate {
+
+		public MainGameUpdate(){
+			update();
+		}
+		
+		/*
+		 *  update()
+		 *  
+		 *  Calls other updates for the game and rendering,
+		 */
+		private void update(){
+			
+		}
+}
